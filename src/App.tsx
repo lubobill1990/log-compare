252a56afb21c81298c4fab36bbf73e8fbf7c2342
Bo Lu <lubobill1990@163.com>
2022-05-20 12:24:39 +0800

    add advertising and user guidance

--- src/App.tsx
+++ src/App.tsx
@@ -275,7 +275,15 @@ function App() {
     <div className="App" ref={dropRef}>
       <div className="vertical-compare-zone">
         {logFiles.size === 0 && (
-          <div className="dropzone-hint">Drop log file here</div>
+          <div className="dropzone-hint">
+            <div>Drop log file here</div>
+            <div className="ensure-hint">
+              To compare log files with timestamp locally, safely, and
+              efficiently.
+              <br />
+              Multiple tabs comparing supported.
+            </div>
+          </div>
         )}
         {Array.from(logFiles.keys()).map((key) => {
           const logFile = logFiles.get(key);
