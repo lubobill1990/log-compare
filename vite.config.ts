c084f2efbf1c921b0f1c867e1a11ce97332a6596
Bo Lu <bolu1@microsoft.com>
2023-03-26 16:00:22 +0800

    add google analytics script

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,24 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { comlink } from 'vite-plugin-comlink';
 import eslint from 'vite-plugin-eslint';
+import { VitePluginRadar } from 'vite-plugin-radar';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), eslint(), comlink()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    eslint(),
+    comlink(),
+    VitePluginRadar({
+      // Google Analytics tag injection
+      enableDev: true,
+      analytics: {
+        id: 'G-RZ5NKLB1W2',
+      },
+    }),
+  ],
   worker: {
     plugins: [comlink(), tsconfigPaths(), eslint()],
   },
